110d6951092b1b070763149815de91278f1a9170
Gagan deep <ext-gagan.deep@here.com>
2025-02-10 11:48:41 +0530

    cors settings

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ app = FastAPI()
 # Configure CORS settings
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5173", "http://localhost:3000"], 
+    allow_origins=["*", "http://127.0.0.1:5173", "http://localhost:3000"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
